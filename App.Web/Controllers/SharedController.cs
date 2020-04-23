3a3a9a245d32ad4f9f5173cac64b799e43342563
Victor Silva <vsilva.inbox@outlook.com>
2020-04-23 11:14:58 -0400

    Se agrega control de error 404

---
+++ App.Web/Controllers/SharedController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace App.Web.Controllers
+{ 
+    public class SharedController : Controller {
+        public ActionResult Error(string message) {
+            ViewBag.Error = message;
+            return View(message);
+        }
+    }
+}
