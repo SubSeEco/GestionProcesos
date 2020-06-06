2d5832aefa171d704662b199844ece02479ffdbc
Victor Silva <vsilva.inbox@outlook.com>
2020-06-05 20:41:57 -0400

    Término desarrollo Informe HSA + Clave Única

--- App.Web/Controllers/InformeHSAController.cs
+++ App.Web/Controllers/InformeHSAController.cs
@@ -4,14 +4,23 @@ using App.Model.Core;
 using App.Model.InformeHSA;
 using App.Core.Interfaces;
 using Rotativa.MVC;
-using System.Web;
-using RestSharp;
-using App.Infrastructure.Email;
 using App.Core.UseCases;
-using System.Net;
+using System.Linq;
 
 namespace App.Web.Controllers
 {
+    public class DTOProceso
+    {
+        public DTOProceso()
+        {
+
+        }
+        public int Id { get; set; }
+        public DateTime Inicio { get; set; }
+        public string Tipo { get; set; }
+        public string Estado { get; set; }
+        public string Tarea { get; set; }
+    }
     public class DTOInformeHSA
     {
         public DTOInformeHSA()
@@ -45,10 +54,11 @@ namespace App.Web.Controllers
         public DateTime FechaBoleta { get; set; }
 
         public byte[] Boleta { get; set; }
-
+        public string BoletaString { get; set; }
+        public string BoletaFilename { get; set; }
+        public string BoletaFiletype { get; set; }
         public string Email { get; set; }
     }
-
 
     [Audit]
     [Authorize]
@@ -184,47 +194,80 @@ namespace App.Web.Controllers
         {
             var persona = _sigper.GetUserByRut(id);
             if (persona == null)
-                return null;
+                return Json(new { ok = false, error = "Error al consultar el sistema SIGPER" }, JsonRequestBehavior.AllowGet);
+
+            if (persona != null && persona.Funcionario == null)
+                return Json(new { ok = false, error = "No se encontró información del funcionario en SIGPER" }, JsonRequestBehavior.AllowGet);
 
             if (persona != null && persona.FunDatosLaborales != null && persona.FunDatosLaborales.RH_ContCod != 10)
-                return null;
+                return Json(new { ok = false, error = "El funcionario no tiene calidad jurídica de honorario a suma alzada" }, JsonRequestBehavior.AllowGet);
 
-            return Json(new
-            {
-                Nombre = persona.Funcionario.PeDatPerChq,
-                Unidad = persona.Unidad.Pl_UndDes,
-                NombreJefatura = persona.Jefatura.PeDatPerChq,
-                Email = persona.Funcionario.Rh_Mail
-            }, JsonRequestBehavior.AllowGet);
+            if (persona != null && persona.FunDatosLaborales == null)
+                return Json(new { ok = false, error = "No se encontró información laboral en SIGPER" }, JsonRequestBehavior.AllowGet);
+
+            if (persona != null && persona.Jefatura == null)
+                return Json(new { ok = false, error = "No se encontró información de la jefatura en SIGPER" }, JsonRequestBehavior.AllowGet);
+
+            if (persona != null && persona.Unidad == null)
+                return Json(new { ok = false, error = "No se encontró información de la unidad en SIGPER" }, JsonRequestBehavior.AllowGet);
+
+            return Json(new { ok = true, error = "", Nombre = persona.Funcionario.PeDatPerChq, Unidad = persona.Unidad.Pl_UndDes, NombreJefatura = persona.Jefatura.PeDatPerChq, Email = persona.Funcionario.Rh_Mail }, JsonRequestBehavior.AllowGet);
         }
 
         [AllowAnonymous]
         public JsonResult StartProcess(DTOInformeHSA model)
         {
+            //validaciones
+            if (string.IsNullOrEmpty(model.Actividades))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar las actividades" }, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(model.BoletaString) || string.IsNullOrEmpty(model.BoletaFilename))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe adjuntar el archivo con la boleta de honorarios" }, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(model.Funciones))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar las funciones" }, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(model.NumeroBoleta))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar el número de boleta" }, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(model.Email))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar el email de funcionario" }, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(model.Nombre))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar el nombre de funcionario" }, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(model.NombreJefatura))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar el nombre de la jefatura" }, JsonRequestBehavior.AllowGet);
+            if (model.RUT == 0)
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar un RUT válido de funcionario" }, JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(model.Unidad))
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso: debe indicar la unidad del funcionario" }, JsonRequestBehavior.AllowGet);
+
             //crear proceso
             var _useCaseCore = new UseCaseCore(_repository, _email, _sigper);
-            var _UseCaseCoreResponseMessage = _useCaseCore.ProcesoInsert(new Proceso {
+            var _UseCaseCoreResponseMessage = _useCaseCore.ProcesoInsert(new Proceso
+            {
                 DefinicionProcesoId = (int)App.Util.Enum.DefinicionProceso.InformeHSA,
                 Email = model.Email
             });
 
             if (!_UseCaseCoreResponseMessage.IsValid)
-            {
-                Response.StatusCode = (int)HttpStatusCode.NotFound;
-                return Json("Problemas al crear proceso", JsonRequestBehavior.AllowGet);
-            }
+                return Json(new { ok = false, id = 0, error = "Problemas al crear proceso" }, JsonRequestBehavior.AllowGet);
 
             //crear workflow
             var workflow = _repository.GetFirst<Workflow>(q => q.ProcesoId == _UseCaseCoreResponseMessage.EntityId);
             if (workflow == null)
+                return Json(new { ok = false, id = 0, error = "Problemas al obtener el workflow inicial" }, JsonRequestBehavior.AllowGet);
+
+            //asociar boleta
+            workflow.Documentos.Add(new Documento
             {
-                Response.StatusCode = (int)HttpStatusCode.NotFound;
-                return Json("Problemas al obtener el workflow inicial", JsonRequestBehavior.AllowGet);
-            }
+                WorkflowId = workflow.WorkflowId,
+                ProcesoId = workflow.ProcesoId,
+                FileName = model.BoletaFilename,
+                Type = model.BoletaFiletype,
+                File = System.Text.Encoding.Default.GetBytes(model.BoletaString),
+                Email = model.Email
+            });
 
             //crear informe hsa
             var _useCaseInformeHSAInteractor = new UseCaseInformeHSA(_repository);
-            var _UseCaseInformeHSAResponseMessage = _useCaseInformeHSAInteractor.Insert(new InformeHSA {
+            var _UseCaseInformeHSAResponseMessage = _useCaseInformeHSAInteractor.Insert(new InformeHSA
+            {
                 Actividades = model.Actividades,
                 ConJornada = model.ConJornada,
                 FechaBoleta = model.FechaBoleta,
@@ -243,21 +286,35 @@ namespace App.Web.Controllers
             });
 
             if (!_UseCaseInformeHSAResponseMessage.IsValid)
-            {
-                Response.StatusCode = (int)HttpStatusCode.NotFound;
-                return Json("Problemas al crear informe HSA", JsonRequestBehavior.AllowGet);
-            }
+                return Json(new { ok = false, id = 0, error = "Problemas al crear informe HSA" }, JsonRequestBehavior.AllowGet);
 
-            //enviar tarea a paso 2
+            //enviar tarea a siguiente paso
             var _UseCaseWorkflowResponseMessage = _useCaseCore.WorkflowUpdate(workflow);
             if (!_UseCaseWorkflowResponseMessage.IsValid)
-            {
-                Response.StatusCode = (int)HttpStatusCode.NotFound;
-                return Json("Problemas al activar la segunda tarea", JsonRequestBehavior.AllowGet);
-            }
+                return Json(new { ok = false, id = 0, error = "Problemas al enviar informe a la siguiente tarea" }, JsonRequestBehavior.AllowGet);
 
-            Response.StatusCode = (int)HttpStatusCode.OK;
-            return Json(_UseCaseCoreResponseMessage.EntityId, JsonRequestBehavior.AllowGet);
+            return Json(new { ok = true, id = _UseCaseCoreResponseMessage.EntityId }, JsonRequestBehavior.AllowGet);
+        }
+
+        [AllowAnonymous]
+        public JsonResult GetInformes(int id)
+        {
+            var persona = _sigper.GetUserByRut(id);
+            if (persona == null)
+                return Json(new { ok = false, error = "Error al consultar el sistema SIGPER" }, JsonRequestBehavior.AllowGet);
+            if (persona != null && persona.Funcionario == null)
+                return Json(new { ok = false, error = "No se encontró información del funcionario en SIGPER" }, JsonRequestBehavior.AllowGet);
+
+            var DTOProcesos = _repository.Get<Proceso>(q => q.Email == persona.Funcionario.Rh_Mail.Trim()).Select(q => new DTOProceso
+            {
+                Id = q.ProcesoId,
+                Inicio = q.FechaCreacion,
+                Tipo = q.DefinicionProceso.Descripcion,
+                Estado = q.EstadoProceso.Descripcion,
+                Tarea = q.Workflows.Any() ? q.Workflows.OrderByDescending(i=>i.WorkflowId).FirstOrDefault().DefinicionWorkflow.Nombre : string.Empty
+            }).ToList();
+
+            return Json(new { ok = true, error = "", DTOProcesos }, JsonRequestBehavior.AllowGet);
         }
     }
 }
