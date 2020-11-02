bbc7f628680e8a5e9ac07613dba318a9ca9cdc5c
Victor Silva <vsilva.inbox@outlook.com>
2020-11-01 22:55:55 -0300

    Paso a producción GD

--- App.Infrastructure/File/File.cs
+++ App.Infrastructure/File/File.cs
@@ -80,6 +80,7 @@ namespace App.Infrastructure.File
                 {
                     var pdfContent = stamper.GetOverContent(1);
                     var pagesize = reader.GetPageSize(1);
+                    ColumnText.ShowTextAligned(pdfContent, iTextSharp.text.Element.ALIGN_MIDDLE,new iTextSharp.text.Phrase("ID", new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 10, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.DARK_GRAY)),pagesize.Width - 35, (pagesize.Height / 2) + 15, 0);
                     ColumnText.ShowTextAligned(pdfContent, iTextSharp.text.Element.ALIGN_MIDDLE,new iTextSharp.text.Phrase(text, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 10, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.DARK_GRAY)),pagesize.Width - 35, pagesize.Height / 2, 0);
                     stamper.Close();
                 }
