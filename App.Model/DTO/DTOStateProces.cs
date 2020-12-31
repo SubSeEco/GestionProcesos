aba3f075d65a6942522348cd31b58022283ff76b
Mauricio Nacor Montoya Barría <mmontoya@economia.cl>
2020-12-30 22:16:46 -0300

    30122020 -correcion en metodo que valida la creacion de resoluciones -se crea dto para visualizar estado del proceso -se crea enumeracion para calculo de valor horas extras -se corrige visualizacion de funcionarios en dropdown de cometidos -se crea metodos para visualizar estado del proceso en horas extras -se crean y modifican vistas en proceso de horas extras

---
+++ App.Model/DTO/DTOStateProces.cs
@@ -0,0 +1,21 @@
+﻿using App.Model.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model.DTO
+{
+    public class DTOStateProces
+    {
+        public DTOStateProces()
+        { }
+        public int Tarea { get; set; }
+        public int Total { get; set; }
+        public float Porcentaje { get; set; }
+        public int Secuencia { get; set; }
+        public List<Workflow> CantTareasRealizadas { get; set; }
+        public List<DefinicionWorkflow> DefWorkflow { get; set; }
+    }
+}
