eac36e0a400fcef82045311094236fdfe1dc046c
Enrique Reyes <ereyes@economia.cl>
2021-01-04 12:32:21 -0300

    Firma Documentos Genericos (Minsegpres)
    Clase Minsegpres
    Interfaz IMinsegpres
    Modelo
    Controlador
    Vista
    Caso de Uso

---
+++ App.Model/FirmaDocumentoGenerico/Root.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model.FirmaDocumentoGenerico
+{
+    public class File
+    {
+        public string content { get; set; }
+        public string status { get; set; }
+        public string contentType { get; set; }
+        public string description { get; set; }
+        public string checksum_original { get; set; }
+        public string documentStatus { get; set; }
+    }
+
+    public class Metadata
+    {
+        public bool otpExpired { get; set; }
+        public int filesSigned { get; set; }
+        public int signedFailed { get; set; }
+        public int objectsReceived { get; set; }
+    }
+
+    public class Root
+    {
+        public List<File> files { get; set; }
+        public Metadata metadata { get; set; }
+        public long idSolicitud { get; set; }
+    }
+}
